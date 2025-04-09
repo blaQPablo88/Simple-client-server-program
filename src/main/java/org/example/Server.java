040ff01d1653028dee6b38bc9e0f952d1c922299
kamogotjhb024 <kamogotjhb024@student.wethinkcode.co.za>
2025-04-09 22:51:19 +0200

    server listening on port 4999

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.io.*;
+import java.net.*;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+        // receives connection
+        ServerSocket serversocket = new ServerSocket(4999);
+        // accepts connection
+        Socket socket = serversocket.accept();
+
+        System.out.println("connection created.");
+    }
 }
