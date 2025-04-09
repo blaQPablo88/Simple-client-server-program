87dc82843b33fa693d51e8185620413aa85a9d3c
kamogotjhb024 <kamogotjhb024@student.wethinkcode.co.za>
2025-04-09 22:37:30 +0200

    creating a TCP connection to a server

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.*;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+        // creating a TCP connection to a server
+        Socket s = new Socket("localhost", 4999);
+    }
 }
